0e110b9e7b3750c4aa86fbf60a24b8d962b7f56b
Eduardo Guerra Alvarez <eduardo.guerraa@alumnos.udg.mx>
2020-04-17 16:27:04 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Proyecto Sistema de Administración de Diseño Gráfico
+
+
+** Descripción **
+
+El proyecto es una aplicacion WEB, programada en el BACKEND con ** PHP ** y el FRONTEND con ** HTML, CSS y BOOTSTRAP **
+
+El proyecto esta ordenado en carpetas
+
+- assets
+En esta parte iran todos los archivos como imagenes y css
+- includes
+En este iran todos los archivos que se incluiran en las vistas
+- views
+En esta carpeta iran todas las vistas que se mostraran
+
+## La base de datos se llamara SADG
+El Host sera ** localhost **
+El usuario sera ** root **
+
+[Bootstrap] (https://getbootstrap.com/docs/4.4/getting-started/introduction/)
+[PHP] (https://www.w3schools.com/php/default.asp)
---
+++ database.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE SADG;
+
+CREATE TABLE empleados(
+    id          INT auto_increment,
+    nombre      VARCHAR(20),
+    apellidos   VARCHAR(30),
+    direccion   VARCHAR(50),
+    telefono    INT(10),
+    correo      VARCHAR(70),
+    salario     FLOAT,
+    rol         VARCHAR(20),
+    CONSTRAINT pk_empleados PRIMARY KEY(id)
+);
+
+CREATE TABLE clientes(
+    id          INT auto_increment,
+    nombre      VARCHAR(20),
+    apellidos   VARCHAR(30),
+    direccion   VARCHAR(50),
+    ciudad      VARCHAR(20),
+    telefono    INT,
+    correo      VARCHAR(70),
+    CONSTRAINT pk_clientes PRIMARY KEY(id)
+);
+
+CREATE TABLE proyectos(
+    id              INT auto_increment,
+    nombre          VARCHAR(50),
+    fecha_inicio    DATE,
+    fecha_final     DATE,
+    coste           FLOAT
+    CONSTRAINT pk_proyectos PRIMARY KEY(id)
+);
